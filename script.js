139b3496846f100cf561e3312606f9d4616cedbc
josecortorreal <josebibby23@gmail.com>
2023-03-23 00:38:27 -0400

    function was added to render forecast weather

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ async function fetchWeatherData(city) {
       console.error(error);
     }
   }
+
+  function renderCurrentWeather(data) {
+    const { name, dt, weather, main, wind } = data;
+    const date = new Date(dt * 1000).toLocaleDateString();
+    const icon = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    const temperature = `${Math.round(main.temp)}°C`;
+    const humidity = `${main.humidity}%`;
+    const windSpeed = `${Math.round(wind.speed)} m/s`;
+  
+    currentWeather.innerHTML = `
+      <h2>${name} - ${date}</h2>
+      <img src="${icon}" alt="${weather[0].description}">
+      <p>Temperature: ${temperature}</p>
+      <p>Humidity: ${humidity}</p>
+      <p>Wind Speed: ${windSpeed}</p>
+    `;
+  }
