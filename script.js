ffbc6040db70535913ae27237cfc8bd9264494c7
josecortorreal <josebibby23@gmail.com>
2023-03-23 00:04:25 -0400

    current weather data was aplied

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ form.addEventListener('submit', e => {
   }
   localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
   renderSearchHistory();
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+    const { name, dt, weather, main, wind } = data;
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const iconCode = weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    const date = new Date(dt * 1000).toLocaleDateString();
