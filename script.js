70c59171212cb218d8dea13664c314c75d5acea4
josecortorreal <josebibby23@gmail.com>
2023-03-23 00:23:00 -0400

    started styling in CSS

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ form.addEventListener('submit', e => {
     const iconCode = weather[0].icon;
     const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
     const date = new Date(dt * 1000).toLocaleDateString();
+
+    card.innerHTML = `
+    <h2>${name}</h2>
+    <div class="date">${date}</div>
+    <img src="${iconUrl}" alt="${weather[0].description}">
+    <div class="temperature">${Math.round(main.temp)}&deg;C</div>
+    <div class="humidity">Humidity: ${main.humidity}%</div>
+    <div class="wind-speed">Wind Speed: ${wind.speed} m/s</div>
+  `;
+
+  currentWeatherDiv.innerHTML = '';
+  currentWeatherDiv.appendChild(card);
+})
+.catch(error => console.error(error));
+
+// Get weather forecast
+fetch
